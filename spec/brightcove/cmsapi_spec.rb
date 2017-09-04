dfa7e96674d014f007c23a891c4a24e43e331d31
Daniel King <daniel.king5@nhs.net>
2017-09-04 11:10:41 +0100

    remove uneccessary gem version spec

--- spec/brightcove/cmsapi_spec.rb
+++ spec/brightcove/cmsapi_spec.rb
@@ -1,10 +1,6 @@
 require "spec_helper"
 
 RSpec.describe Brightcove::Cmsapi do
-  it "has a version number" do
-    expect(Brightcove::Cmsapi::VERSION).not_to be nil
-  end
-
   before do
     stub_request(:post, Brightcove::Cmsapi::OAUTH_ENDPOINT)
       .to_return(
